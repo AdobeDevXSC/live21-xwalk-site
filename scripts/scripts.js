1912e252e1c3544b1fb3b13e0a89cdd2db5dc8be
deck-adobe <deckreyes@adobe.com>
2025-04-02 10:09:05 -0700

    reverting back.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -57,63 +57,6 @@ export function getAllMetadata(scope) {
   }, {});
 }
 
-export function getSiteRoot(level = 3, path = window.location.pathname) {
-  return path.split(/[/.]/, level).join('/');
-}
-
-function patchDemoBlocks(config) {
-  if (window.wknd.demoConfig.blocks && window.wknd.demoConfig.blocks[config.blockName]) {
-    const url = window.wknd.demoConfig.blocks[config.blockName];
-    const splits = new URL(url).pathname.split('/');
-    const [, owner, repo, , branch] = splits;
-    const path = splits.slice(5).join('/');
-
-    const franklinPath = `https://little-forest-58aa.david8603.workers.dev/?url=https://${branch}--${repo}--${owner}.hlx.live/${path}`;
-    return {
-      ...config,
-      jsPath: `${franklinPath}/${config.blockName}.js`,
-      cssPath: `${franklinPath}/${config.blockName}.css`,
-    };
-  }
-  return (config);
-}
-
-async function loadDemoConfig() {
-  const demoConfig = {};
-  const pathSegments = window.location.pathname.split('/');
-  if (window.location.pathname.startsWith('/drafts/') && pathSegments.length > 4) {
-    const demoBase = pathSegments.slice(0, 4).join('/');
-    const resp = await fetch(`${demoBase}/theme.json?sheet=default&sheet=blocks&`);
-    if (resp.status === 200) {
-      const json = await resp.json();
-      const tokens = json.data || json.default.data;
-      const root = document.querySelector(':root');
-      tokens.forEach((e) => {
-        root.style.setProperty(`--${e.token}`, `${e.value}`);
-        demoConfig[e.token] = e.value;
-      });
-      demoConfig.tokens = tokens;
-      demoConfig.demoBase = demoBase;
-      const blocks = json.blocks ? json.blocks.data : [];
-      demoConfig.blocks = {};
-      blocks.forEach((block) => {
-        demoConfig.blocks[block.name] = block.url;
-      });
-
-      window.hlx.patchBlockConfig.push(patchDemoBlocks);
-    }
-
-    if (!demoConfig.demoBase) {
-      const navCheck = await fetch(`${demoBase}/nav.plain.html`);
-      if (navCheck.status === 200) {
-        demoConfig.demoBase = demoBase;
-      }
-    }
-  }
-  window.wknd = window.wknd || {};
-  window.wknd.demoConfig = demoConfig;
-}
-
 /**
  * Returns the current timestamp used for scheduling content.
  */
