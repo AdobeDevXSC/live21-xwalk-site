17941d1bd7023b11f98928296620bfdda52765a9
deck-adobe <deckreyes@adobe.com>
2025-04-03 16:47:42 -0700

    Fixing published URL

--- blocks/content-fragment/content-fragment.js
+++ blocks/content-fragment/content-fragment.js
@@ -6,8 +6,8 @@ import { isAuthorEnvironment, moveInstrumentation } from '../../scripts/scripts.
  * @param {Element} block
  */
 export default async function decorate(block) {
-  const aemauthorurl = getMetadata('authorurl') || '';
-  const aempublishurl = getMetadata('publishurl') || '';
+  const aemauthorurl = 'https://author-p46820-e1400905.adobeaemcloud.com' || '';
+  const aempublishurl = 'https://publish-p46820-e1400905.adobeaemcloud.com/' || '';
   const persistedquery = '/graphql/execute.json/aem-demo-assets/offerByPath';
   const contentPath = block.querySelector(':scope div:nth-child(1) > div a')?.textContent?.trim();
   const variationname =
