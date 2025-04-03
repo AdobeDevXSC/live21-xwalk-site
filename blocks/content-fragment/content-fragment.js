e63e65b4a0c00591ac7466fdc17a080a27391456
deck-adobe <deckreyes@adobe.com>
2025-04-03 16:49:31 -0700

    Fixes for edge URL

--- blocks/content-fragment/content-fragment.js
+++ blocks/content-fragment/content-fragment.js
@@ -7,7 +7,7 @@ import { isAuthorEnvironment, moveInstrumentation } from '../../scripts/scripts.
  */
 export default async function decorate(block) {
   const aemauthorurl = 'https://author-p46820-e1400905.adobeaemcloud.com' || '';
-  const aempublishurl = 'https://publish-p46820-e1400905.adobeaemcloud.com/' || '';
+  const aempublishurl = 'https://publish-p46820-e1400905.adobeaemcloud.com' || '';
   const persistedquery = '/graphql/execute.json/aem-demo-assets/offerByPath';
   const contentPath = block.querySelector(':scope div:nth-child(1) > div a')?.textContent?.trim();
   const variationname =
