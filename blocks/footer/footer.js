78eb4c45b6f9c12e6c94898768218ef3aa4f8d0a
deck-adobe <deckreyes@adobe.com>
2025-04-03 18:00:16 -0700

    Footer fixing

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -14,8 +14,8 @@ export default async function decorate(block) {
   let footerURL = `${getSiteRoot(4)}/footer.plain.html`;
   let updatedFooterUrl = footerURL.replace(/about-us\/|faqs\/|index-demo\/|magazine\/.+\/|adventures\/.+\//g, "/");
 
-  const resp = await fetch(updatedFooterUrl.replace("//", "/"), window.location.pathname.endsWith('/footer') ? { cache: 'reload' } : {});
-  /*const resp = await fetch(`${footerPath}.plain.html`);*/
+  //const resp = await fetch(updatedFooterUrl.replace("//", "/"), window.location.pathname.endsWith('/footer') ? { cache: 'reload' } : {});
+  const resp = await fetch(`${footerPath}.plain.html`);
 
   if (resp.ok) {
     block.textContent = '';
