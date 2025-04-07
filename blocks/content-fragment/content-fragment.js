9ba080d4342c5da1f660c69d1ed8a0510a938629
deck-adobe <deckreyes@adobe.com>
2025-04-07 09:42:26 -0700

    CORS fixes

--- blocks/content-fragment/content-fragment.js
+++ blocks/content-fragment/content-fragment.js
@@ -29,7 +29,7 @@ export default async function decorate(block) {
         credentials: 'include'
       };
  
-  const cfReq = await fetch(url, options)
+  const cfReq = await fetch(url)
     .then((response) => response.json())    
     .then((contentfragment) => {
       let offer = '';
